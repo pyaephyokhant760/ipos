ce06452068c82d8d4e9f6f5b968601d41b28e9cb
pyaephyokhant760 <pyaephyokhant2020dc@gmail.com>
2026-07-07 01:01:28 +0630

    create_project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+import express from "express"; 
+ 
+require("dotenv").config();
+const cors = require("cors");
+const app = express(); 
+ 
+app.use(cors()); 
+ 
+const PORT = process.env.PORT || 8000; 
+ 
+app.use(express.json());
+ 
+app.listen(PORT, () => {
+  // Start the server and listen on the specified port
+  console.log(`Server is running on http://localhost:${PORT}`); // Log a message indicating the server is running
+});
